b9c3534565c04fd37c22aca6a27cf989c44145c8
Joe-Copland <joe.copland@bbc.co.uk>
2020-11-03 12:11:01 +0000

    Added app for source machine file

---
+++ measure_time_source.cpp
@@ -0,0 +1,23 @@
+#include <chrono>
+#include <cstdint>
+#include <iostream>
+#include <fstream>
+
+uint64_t timeSinceEpochMicrosec() {
+  using namespace std::chrono;
+  // Returns time in microseconds
+  return duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+}
+
+int main() {
+  //Records time
+  uint64_t timeStamp = timeSinceEpochMicrosec();
+  std::ofstream timeLog("/home/ec2-user/file_source/time_log_file.csv");
+  // Send data to the stream
+  timeLog << timeStamp << "\n";
+  // Close the file
+  timeLog.close();
+  return 0;
+}
+
+
