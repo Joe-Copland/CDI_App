0382b6813e7dc31d6c97c3b5ee96b745ec748753
Joe-Copland <joe.copland@bbc.co.uk>
2020-11-05 15:19:15 +0000

    Finished functionality

--- measure_time_sink.c
+++ measure_time_sink.c
@@ -20,7 +20,7 @@ char * getScpCommand() {
   //Takes input from command line
   scanf("%s", DNSName);
   //Repeats input
-  printf( "You entered: %s %s", DNSName, "\n");
+  //printf( "You entered: %s %s", DNSName, "\n");
   //Form string for system() command, create memory allocation of large enough size
   char *scpCommand = malloc(200 * sizeof(char));
   if(scpCommand == NULL) return NULL;
@@ -29,35 +29,40 @@ char * getScpCommand() {
   //Combine three parts into scpCommand
   snprintf(scpCommand,200,"%s%s%s",scpCommandStart,DNSName,scpCommandEnd);
   //Prints command
-  printf( "The SCP command is: %s%s",scpCommand,"\n");
+  //printf( "The SCP command is: %s%s",scpCommand,"\n");
   return scpCommand;
 }
 
 //Gets start time from time log file
-//uint64_t getStartTime(std::string scpCommandString) {
-  //converts to correct type
-  //const char * scp = scpCommandString.c_str();
+u_int64_t getStartTime(char * scpCommand) {
   //runs command
-  //system(scp);
+  u_int64_t timeStart;
+  system(scpCommand);
+  //int got = fscanf("/home/ec2-user/file_sink/time_log_file.csv", "%lu", timeStart);
+  FILE * fp;
+  fp = fopen ("file.txt", "w+");
+  //rewind(fp);
+  fscanf(fp, "%lu", timeStart);
+  fclose(fp);
+
   //get start time from file
   //std::fstream fin;
   //fin.open("/home/ec2-user/file_sink/time_log_file.csv");
   //std::string timeStart;
   //std::getline(fin,timeStart);
   //uint64_t timeStartInt = std::stoll(timeStart);
-  //return timeStartInt;
-//}
+  return timeStart;
+}
 
 int main() {
+  u_int64_t timeEnd = CdiOsGetMicroseconds();
   //Gets scp command string
   char * scpCommand = getScpCommand();
-  printf("Returned command is: %s%s", scpCommand,"\n");
-  //Free up memory allocation
+  //printf("Returned command is: %s%s", scpCommand,"\n");
+  u_int64_t timeStart = getStartTime(scpCommand);
+  printf("Start time is: %lu%s", timeStart, "\n");
   free(scpCommand);
-  //Records time
-  //u_int64_t timeEnd = timeSinceEpochMicrosec();
-  //uint64_t timeStart = getStartTime(scpCommandString);
-  //Prints out latency
-  //std::cout << timeEnd-timeStart << "\n";
+  //Free up memory allocation
+  printf("Latency is: %lu%s",timeEnd-timeStart," microseconds!\n");
   return 0;
 }
