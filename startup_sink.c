3749c4aef3d73ec348d7c7dc4d7b6a43d09af14c
Joe-Copland <joe.copland@bbc.co.uk>
2020-11-09 14:49:56 +0000

    fixed scp bugs

--- startup_sink.c
+++ startup_sink.c
@@ -15,7 +15,7 @@ char * getScpCommand() {
   char *scpCommand = malloc(200 * sizeof(char));
   if(scpCommand == NULL) return NULL;
   char scpCommandStart[] = "scp -i /home/ec2-user/joec_cdi_ireland.pem ec2-user@";
-  char scpCommandEnd[] = ":/home/ec2-user/file_source/test_connection.txt /home/ec2-user/file_sink/";
+  char scpCommandEnd[] = ":/home/ec2-user/file_source/time_log_file.csv /home/ec2-user/file_sink/";
   //Combine three parts into scpCommand
   snprintf(scpCommand,200,"%s%s%s",scpCommandStart,DNSName,scpCommandEnd);
   return scpCommand;
@@ -32,7 +32,7 @@ void saveScp(char * scpCommand){
 //opening connection
 void startTest(){
   //Forming startup command
-  char testCommand[200];
+  char testCommand[600];
   char testCommandStart[] = "./build/debug/bin/cdi_test --adapter EFA --local_ip ";
   char testCommandEnd[] = " --stats_cloudwatch CDIStats eu-west-1 Stream1 -X --connection_name joec_one   --rx AVM --dest_port 2000 --rate 60 --num_transactions 10000 -S --id 1 --payload_size 5184000 --pattern INC --avm_video 1920 1080 YCbCr422 Unused 10bit 60 1 BT2020 true false PQ Narrow 16 9 0 1080 0 0";
   //getting receiver IP
@@ -41,7 +41,7 @@ void startTest(){
   printf(receiverIPQuestion);
   scanf("%s", receiverIP);
   //Combining bits of command
-  snprintf(testCommand,200,"%s%s%s",testCommandStart,receiverIP,testCommandEnd);
+  snprintf(testCommand,600,"%s%s%s",testCommandStart,receiverIP,testCommandEnd);
   printf("Hold onto your hats, we're going in!\n");
   //Opening connection
   system(testCommand);
