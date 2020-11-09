c066095622c41e5253328afbee1cb8ff47e80bb9
Joe-Copland <joe.copland@bbc.co.uk>
2020-11-09 15:57:43 +0000

    Changed path so sdk can be run from any directory

--- startup_source.c
+++ startup_source.c
@@ -5,7 +5,7 @@
 void startTest(){
   //Making big command
   char testCommand[200];
-  char testCommandStart[] = "./build/debug/bin/cdi_test --adapter EFA --local_ip ";
+  char testCommandStart[] = "/home/ec2-user/aws-cdi-sdk/build/debug/bin/cdi_test --adapter EFA --local_ip ";
   char testCommandMiddle[] = " -X --tx AVM --remote_ip ";
   char testCommandEnd[] = " --dest_port 2000 --rate 60 --num_transactions 10000 -S --id 1 --payload_size 5184000 --pattern INC --avm_video 1920 1080 YCbCr422 Unused 10bit 60 1 BT2020 true false PQ Narrow 16 9 0 1080 0 0";
   char receiverIP[11];
