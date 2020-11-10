745b7cb9b08c1342b3638ab1eb33188da7f2c61f
Joe-Copland <joe.copland@bbc.co.uk>
2020-11-10 11:22:14 +0000

    made it work

--- store_time_sink.c
+++ store_time_sink.c
@@ -1,60 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <pthread.h>
 
-//Returns current time in microseconds, compared with start of epoch, an inbuil$
+//Returns current time in microseconds, compared with start of epoch
 u_int64_t CdiOsGetMicroseconds() {
   // Returns time in microseconds
-  u_int64_t currentTime = 28;
+  u_int64_t currentTime = 97;
   return currentTime;
 }
 
-//Gets start time from time log file
-u_int64_t getStartTime() {
-  //Run command in shell
-  char scpCommand[200];
-  //Open file
-  FILE *fp;
-  if ((fp = fopen("/home/ec2-user/file_sink/scpCommand.txt","r")) == NULL){
-    printf("Error! opening file");
-    // Program exits if the file pointer returns NULL.
-    exit(1);
-  }
-  //fscanf(fp,"%[^\n]s", &scpCommand);
-  fgets(scpCommand,200,fp);
+void recordTime(){
+  //Records time
+  u_int64_t timeStamp = CdiOsGetMicroseconds();
+  //Opens file
+  FILE * fp;
+  fp = fopen ("/home/ec2-user/file_sink/end_time_log.csv", "a");
+  //Writes timestamp to file
+  fprintf(fp, "%lu%s", timeStamp,",");
   fclose(fp);
-  printf("%s%s",scpCommand,"\n");
-  system(scpCommand);
-  u_int64_t timeStart;
-  //Open file
-  FILE *fptr;
-  if ((fptr = fopen("/home/ec2-user/file_sink/time_log_file.csv","r")) == NULL){
-    printf("Error! opening file");
-    // Program exits if the file pointer returns NULL.
-    exit(1);
-  }
-  fscanf(fptr,"%lu", &timeStart);
-  fclose(fptr);
-  return timeStart;
 }
 
-void *recordTime(void *vargp){
-  //Record finish time
-  u_int64_t timeEnd = CdiOsGetMicroseconds();
-  //Get start time
-  u_int64_t timeStart = getStartTime();
-  //Print difference between start and end times for latency
-  u_int64_t latency = timeEnd-timeStart;
-  printf("Latency is: %lu%s",latency," microseconds!\n");
-  return NULL;
+void clearFile(){
+  FILE * fp;
+  fp = fopen ("/home/ec2-user/file_sink/end_time_log.csv", "w+");
+  fclose(fp);
 }
 
 int main() {
-  pthread_t thread_id;
-  pthread_create(&thread_id, NULL, recordTime, NULL);
-  pthread_join(thread_id, NULL);
+  clearFile();
+  recordTime();
   return 0;
 }
-
-
