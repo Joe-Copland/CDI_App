84e1136634ba351cb80cccdced2f25821d19693c
Joe-Copland <70574557+Joe-Copland@users.noreply.github.com>
2020-11-11 16:37:40 +0000

    changed overwrite to append

--- get_time_sink.cpp
+++ get_time_sink.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <unistd.h>
+#include <string>
 
 //Returns current time in microseconds, compared with start of epoch
 uint64_t timeSinceEpochMicrosec() {
@@ -11,15 +12,21 @@ uint64_t timeSinceEpochMicrosec() {
   return duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
 }
 
+// Usage example: filePutContents("./yourfile.txt", "content", true);
+void filePutContents(const std::string& name, const std::string& content, bool append = false) {
+    std::ofstream outfile;
+    if (append)
+        outfile.open(name, std::ios_base::app);
+    else
+        outfile.open(name);
+    outfile << content;
+}
+
 void recordTime(){
   //Records time
   uint64_t timeStamp = timeSinceEpochMicrosec();
-  //opens file
-  std::ofstream timeLog("/home/ec2-user/file_sink/end_time_log.csv");
-  // Send time to file
-  timeLog << timeStamp << ",";
-  // Closes the file
-  timeLog.close();
+  std::string timeStampString = std::to_string(timeStamp) + ",";
+  filePutContents("/home/ec2-user/file_sink/end_time_log.csv", timeStampString, true);
 }
 
 int main() {
