c58c5871718c35ae5b06d0e2f867be75635b0f9e
Joe-Copland <70574557+Joe-Copland@users.noreply.github.com>
2020-11-11 13:49:41 +0000

    Set up code, it's basically the same as clock_sync_source

---
+++ get_time_source.cpp
@@ -0,0 +1,28 @@
+#include <chrono>
+#include <cstdint>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+
+//Returns current time in microseconds, compared with start of epoch
+uint64_t timeSinceEpochMicrosec() {
+  using namespace std::chrono;
+  // Returns time in microseconds
+  return duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+}
+
+void recordTime(){
+  //Records time
+  uint64_t timeStamp = timeSinceEpochMicrosec();
+  //opens file
+  std::ofstream timeLog("/home/ec2-user/file_source/start_time_log.csv");
+  // Send time to file
+  timeLog << timeStamp << ",";
+  // Closes the file
+  timeLog.close();
+}
+
+int main() {
+  recordTime();
+  return 0;
+}
