ec2e210fa3f99296c0d6f6ddbf3a07802fb1aee1
Joe-Copland <70574557+Joe-Copland@users.noreply.github.com>
2020-11-13 10:39:27 +0000

    Update latency_plot.py

--- latency_plot.py
+++ latency_plot.py
@@ -12,25 +12,30 @@ payload_size=5184000
 
 latency=np.zeros(len(start_t))
 packet_no=np.linspace(1,len(start_t),len(start_t))
+packet_no2=np.linspace(1,len(start_t)-1,len(start_t)-1)
 
 for i in range(len(start_t)):
     latency[i]=(end_t[i]-start_t[i])/1000
 
-network_speed=np.zeros(len(start_t))
-for i in range(len(start_t)):
-    network_speed[i]=(payload_size/1000000)/(latency[i]/1000)
+trt=np.zeros(len(start_t)-1)
+    
+for i in range(len(start_t)-1):
+    trt[i]=(start_t[i+1]-start_t[i])/1000
 
-"""
-pyplot.figure()
-pyplot.plot(packet_no,latency)
-pyplot.xlim(0,100)
-pyplot.xlabel("Packets Sent")
-pyplot.ylabel("Latency/ms")
-pyplot.show()
-"""
-pyplot.figure()
-pyplot.plot(packet_no,network_speed)
-pyplot.xlim(0,len(start_t))
-pyplot.xlabel("Payloads Sent")
-pyplot.ylabel("Network Speed/Mbs$^-$$^1$")
-pyplot.savefig('network_speed_plot.png')
+network_speed=np.zeros(len(start_t)-1)
+for i in range(len(start_t)-1):
+    network_speed[i]=(payload_size/1000000)/(trt[i]/1000)
+
+fig, axs = plt.subplots(2,figsize=(5,7))
+
+axs[0].plot(packet_no,latency)
+axs[0].set_xlabel("Payloads Sent")
+axs[0].set_xlim(0,len(start_t))
+axs[0].set_ylabel("Latency/ms")
+
+axs[1].plot(packet_no2,network_speed)
+axs[1].set_xlabel("Payloads Sent")
+axs[1].set_xlim(0,len(start_t))
+axs[1].set_ylabel("Network Speed/Mbs$^-$$^1$")
+
+plt.savefig('network_speed_plot.png')
