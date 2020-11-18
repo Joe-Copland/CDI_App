2ef4d837600686bca9dc16d6a6c1f7f1802ae4b2
Joe-Copland <70574557+Joe-Copland@users.noreply.github.com>
2020-11-18 10:16:05 +0000

    Update latency_plot.py

--- latency_plot.py
+++ latency_plot.py
@@ -6,7 +6,7 @@ from scipy.stats import sem
 
 print("---------------------PLOTS TIME---------------------------------------------")
 
-number_of_tests=100
+number_of_tests=80
 
 #Loading in timing data from test
 
