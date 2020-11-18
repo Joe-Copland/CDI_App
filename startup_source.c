33ef82731d20d9909117e6bf399f84282dda3737
Joe-Copland <70574557+Joe-Copland@users.noreply.github.com>
2020-11-18 11:10:27 +0000

    Update startup_source.c

--- startup_source.c
+++ startup_source.c
@@ -3,21 +3,24 @@
 #include <string.h>
 #include <unistd.h>
 
+//Clears start_time_log file
 void clearFile(){
   FILE * fp;
   fp = fopen ("/home/ec2-user/file_source/start_time_log.csv", "w+");
   fclose(fp);
 }
 
+//Runs a test
 void startTest(char * receiverIP, char * transmitterIP, int frameRate){
-  //Getting pixel parameters to vary
+  //Works out payload size from framerate input
   signed long payloadSize = frameRate * 86400;
+  //Converts to strings
   char frameRateString[20];
   char payloadSizeString[20];
   sprintf(frameRateString, "%d", frameRate);
   sprintf(payloadSizeString, "%li", payloadSize);
   
-  //Making big command
+  //Makes bits of command
   char testCommand[600];
   char testCommandOne[] = "/home/ec2-user/aws-cdi-sdk/build/debug/bin/cdi_test --adapter EFA --local_ip ";
   char testCommandTwo[] = " -X --tx AVM --remote_ip ";
@@ -25,7 +28,7 @@ void startTest(char * receiverIP, char * transmitterIP, int frameRate){
   char testCommandFour[] = " --pattern INC --avm_video 1920 1080 YCbCr422 Unused 10bit ";
   char testCommandFive[] = " 1 BT2020 true false PQ Narrow 16 9 0 1080 0 0";
  
-  //Combining bits of command
+  //Combines bits of command
   snprintf(testCommand,600,"%s%s%s%s%s%s%s%s%s",testCommandOne,transmitterIP,testCommandTwo,receiverIP,testCommandThree,payloadSizeString,testCommandFour,frameRateString,testCommandFive);
   printf("Hold onto your hats, we're going in!\n");
   //Running command
@@ -46,7 +49,7 @@ int main(){
   printf(transmitterIPQuestion);
   scanf("%s", transmitterIP);
   
-  //Starting test
+  //Starting tests in order
   int i;
   for (i = 1; i < 71; ++i)
   {
@@ -54,6 +57,7 @@ int main(){
     printf("Running test! \n");
     int frameRate = i * 5;
     startTest(receiverIP, transmitterIP, frameRate);
+    //Waiting to ensure code finishes running on sink end before starting the next test
     sleep(5);
   }
   
